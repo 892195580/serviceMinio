533b1b6dfd341569b41078c48f0e25c55c19b10c
root <root@master.localdomain>
2022-08-29 14:41:24 +0800

    change config

--- config/config.go
+++ config/config.go
@@ -1,12 +1,12 @@
 package config
 
 const (
-	endpoint = "play.min.io"
-	accessKeyID = "Q3AM3UQ867SPQQA43P2F"
-	secretAccessKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
-	//endpoint ="192.168.36.131:9000"
-	//accessKeyID = "nKNB72xEmN7A01Wz"
-	//secretAccessKey = "6mTdpUnPqHp5Hwc2ZO8LDLApsVKeBWJ8"
+	//endpoint = "play.min.io"
+	//accessKeyID = "Q3AM3UQ867SPQQA43P2F"
+	//secretAccessKey = "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	endpoint ="192.168.36.131:9000"
+	accessKeyID = "nKNB72xEmN7A01Wz"
+	secretAccessKey = "6mTdpUnPqHp5Hwc2ZO8LDLApsVKeBWJ8"
 	useSSL = true
 )
 
