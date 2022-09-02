073e4eaa160e4e4759dcca058338abbae0852783
朱安东 <892195580@qq.com>
2022-09-02 18:21:06 +0800

    -p sh

--- main_test.sh
+++ main_test.sh
@@ -2,10 +2,10 @@
 echo Test all programe of minio
 mgo minio bucket ls
 mgo minio bucket make apptest
-mgo minio object putf apptest test.jpg ./test.jpg
+mgo minio object putf apptest test.jpg -p ./test.jpg
 mgo minio object ls apptest
 mgo minio object stat apptest test.jpg
-mgo minio object getf apptest test.jpg ./test1.jpg
+mgo minio object getf apptest test.jpg -p ./test1.jpg
 mgo minio object rm apptest test.jpg
 mgo minio object ls apptest
 mgo minio bucket rm apptest
